e796f1d4cafe9700e9ca327c655ff91a023cfbb2
MAXPOL <maxrol32@gmail.com>
2023-03-15 23:11:05 +0300

    Update dbconnect.php

--- dbconnect.php
+++ dbconnect.php
@@ -6,5 +6,8 @@ $dbname = "site";
 
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
 
 ?>
