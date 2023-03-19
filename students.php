5fc9f891e7f06727c817640890d954b4e6f7014a
MAXPOL <maxrol32@gmail.com>
2023-03-19 23:51:26 +0300

    Update students.php

--- students.php
+++ students.php
@@ -6,7 +6,19 @@ require "dbconnect.php";
 
 echo "<center><h2>STUDENTS LIST</h2>"
 
+$query = "SELECT * FROM students";
 
+$result = $conn->query($query);
+
+
+while ($row = mysqli_fetch_assoc($result)) {
+
+  echo "<table><tr>";
+  echo "<td>$row[firstname]</td>";
+  echo "<td>$row[secondname]</td>";
+  echo "<td>$row[surename]</td>";
+
+  }
 
 ?>
 
